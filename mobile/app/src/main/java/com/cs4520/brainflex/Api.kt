e0c4eb695f8b0056239664b55c3fa260f9fc3f6a
Jackson Lawrence <jlawrence1787@gmail.com>
2024-04-01 17:46:58 -0400

    added android starter app

---
+++ mobile/app/src/main/java/com/cs4520/brainflex/Api.kt
@@ -0,0 +1,6 @@
+package com.cs4520.assignment4
+
+object Api {
+    const val BASE_URL: String = "https://kgtttq6tg9.execute-api.us-east-2.amazonaws.com/"
+    const val ENDPOINT: String = "prod/random/"
+}
