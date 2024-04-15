a9536313f4ba2fc9773264df0ebf065dd17f6c19
yunip20 <yunipp20@gmail.com>
2024-04-15 19:36:41 -0400

    sequential animation not working

--- mobile/app/src/main/java/com/cs4520/brainflex/Game.kt
+++ mobile/app/src/main/java/com/cs4520/brainflex/Game.kt
@@ -2,23 +2,30 @@ package com.cs4520.brainflex
 
 import android.util.Log
 import androidx.compose.animation.Animatable
-import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationEndReason
+import androidx.compose.animation.core.AnimationResult
 import androidx.compose.animation.core.AnimationVector4D
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Transition
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.isFinished
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.repeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -26,8 +33,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,96 +45,101 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
-fun GameScreen(viewModel: GameViewModel, navHostController: NavHostController,) {
+fun GameScreen(viewModel: GameViewModel, navHostController: NavHostController) {
 
     val currentLevel by viewModel.currentLevel.observeAsState(1)
-    val sequence by viewModel.sequence.observeAsState(listOf<Int>())
+    val sequence by viewModel.sequence.observeAsState(listOf())
     val gameState by viewModel.gameState.observeAsState(GameState.IN_PROGRESS)
 
-    // TODO: Change this to leaderboard
-    if (gameState == GameState.GAME_OVER) {
-        navHostController.navigate(Screen.LOGIN.name)
-    }
-    Log.d("SEQUENCE", sequence.toString())
     Surface( color = MaterialTheme.colors.background) {
-        Column (horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(30.dp)){
-            Level(gameState, currentLevel)
-            Spacer(modifier = Modifier.height(20.dp))
-            MemorySequenceGame(viewModel, sequence)
-        }
+            Column (horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(30.dp)){
+                Level(gameState, currentLevel)
+                Spacer(modifier = Modifier.height(20.dp))
+                Log.d("SEQUENCE HERE2", sequence.toString())
+                MemorySequenceGame(viewModel, sequence, gameState, navHostController)
+            }
+
     }
 }
 @Composable
 fun Level(gameState: GameState, currentScore: Int) { 
     Text("LEVEL " + currentScore, color = MaterialTheme.colors.primary, fontSize = 20.sp) 
 }
-@Composable
-fun BlinkingCard(viewModel: GameViewModel, index: Int, delay: Long) {
 
+
+@Composable
+fun MemorySequenceGame(
+    viewModel: GameViewModel,
+    sequence: List<Int>,
+    gameState: GameState,
+    navHostController: NavHostController,
+) {
     val blue = MaterialTheme.colors.onBackground
-    val color = remember { Animatable(blue) }
-
-    LaunchedEffect(Unit) {
-        color.animateTo(
-            targetValue = Color.White,
-            animationSpec = tween(durationMillis = 2000)
-        )
-        color.animateTo(
-            targetValue = blue,
-            animationSpec = tween(durationMillis = 2000)
-        )
-        if (delay > 0){
-            kotlinx.coroutines.delay(delay)
-        }
-
+    val flash = MaterialTheme.colors.primary
+    var isDone by remember { mutableStateOf(false) }
+    val flashAnimationSpec = keyframes<Color> {
+        durationMillis = 1000
+        blue at 0  // Initial color
+        flash at 500
+        blue at 1000 // Full white color at 500ms
     }
-    Card(
-        modifier = Modifier
-            .size(80.dp)
-            .padding(5.dp)
-            .clickable{ viewModel.nextStep(index) },
-        backgroundColor = color.value
-    ){}
-}
 
-@Composable
-fun NormalCard(viewModel: GameViewModel, index: Int) {
-    Card( modifier = Modifier
-        .size(80.dp)
-        .padding(5.dp)
-        .clickable{ viewModel.nextStep(index) },
-        backgroundColor = MaterialTheme.colors.onBackground){}
-}
-
-@Composable
-fun MemorySequenceGame(viewModel: GameViewModel, data: List<Int>) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("Observe", color= MaterialTheme.colors.primary,
-            fontSize = 12.sp, textAlign = TextAlign.Center)
+
+        Text(
+            "Observe", color = MaterialTheme.colors.primary,
+            fontSize = 12.sp, textAlign = TextAlign.Center
+        )
         Spacer(modifier = Modifier.height(10.dp))
+
+        val animatables = remember {
+            List(9) {
+                Animatable(blue)
+            }
+        }
+// Start sequential animation
+        LaunchedEffect(Unit) {
+            // Iterate through each index and animate sequentially
+            intArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8).forEachIndexed { index, item ->
+               Log.d("HDHDHDHDHDH", "HDHD")
+                // Animate the current item
+                val animation = animatables[0].animateTo(flash, flashAnimationSpec)
+                Log.d("HDHDHDHDHDH", animation.endState.isFinished.toString())
+                if (index == 8) {
+                    Log.d("TRUE", index.toString())
+                    isDone = true
+                }
+            }
+        }
         LazyVerticalGrid(
             columns = GridCells.Fixed(3),
+            modifier = Modifier.fillMaxSize(),
         ) {
+
             items(9) { index ->
-                if(data.contains(index)){
-                    // the list of sequence is stored as indices of the card
-                    // for instance, if the center cell of the board were to be the first sequence
-                    // the  "data"  or list of sequence would contain 4
-                    // considering that the board looks like this:
-                    // 0 1 2
-                    // 3 4 5
-                    // 6 7 8
-                    // SO if "data" contains it, the cell should blink:
-                    BlinkingCard(viewModel, index, index * 5L)
-                } else {
-                    NormalCard(viewModel, index)
-                }
+                Box(
+                    modifier = Modifier
+                        .size(80.dp)
+                        .padding(5.dp)
+                        .background(animatables[index].value)
+                        .clickable(enabled = isDone) {
+                            viewModel.nextStep(index)
+                            if (gameState == GameState.GAME_OVER) {
+                                viewModel.resetGame()
+                                navHostController.navigate(Screen.INFORMATION.name)
+                            }
+                        }
+                ) {}
             }
         }
     }
 }
+
--- mobile/app/src/main/java/com/cs4520/brainflex/GameViewModel.kt
+++ mobile/app/src/main/java/com/cs4520/brainflex/GameViewModel.kt
@@ -1,10 +1,12 @@
 package com.cs4520.brainflex
 
+import android.util.Log
 import androidx.compose.ui.unit.Constraints.Companion.Infinity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.work.WorkManager
+import kotlin.math.exp
 import kotlin.random.Random
 
 enum class GameState {
@@ -17,12 +19,12 @@ class GameViewModel() : ViewModel() {
     private var _currentLevel = MutableLiveData<Int>(1)
     val currentLevel: LiveData<Int> = _currentLevel
 
-    private val _sequence = MutableLiveData<List<Int>>()
+    private val _sequence = MutableLiveData<List<Int>>(listOf())
     val sequence: LiveData<List<Int>> = _sequence
 
     private val _gameState = MutableLiveData<GameState>(GameState.IN_PROGRESS)
     val gameState: LiveData<GameState> = _gameState
-    private var nextExpectedIndex = 0
+    private var expectedIndex = 0
 
     init {
         generateSequence(currentLevel.value!!)
@@ -33,30 +35,32 @@ class GameViewModel() : ViewModel() {
         _sequence.value =  List(currentLevel) {
             random.nextInt(9) // Generates random number from 0 to 8
         }
+        Log.d("current level", currentLevel.toString())
+        Log.d("SEQUENCE GET UPDATED?", sequence.value.toString())
     }
 
-    fun checkUserInput(cellNumber: Int): Boolean {
-        if(cellNumber == (sequence.value?.get(nextExpectedIndex) ?: Infinity)){
-            nextExpectedIndex++
-            if (nextExpectedIndex == sequence.value?.size) {
+    fun nextStep(cellNumber: Int) {
+        Log.d("What I Click", cellNumber.toString())
+        if(cellNumber == (sequence.value?.get(expectedIndex) ?: Infinity)){
+            Log.d("AM I HERE", sequence.value?.get(expectedIndex).toString())
+            if (expectedIndex == (sequence.value?.size?.minus(1) ?: 0)) {
                 // entire sequence was entered correctly
                 // reset  user input progress for the next round
-                nextExpectedIndex = 0
-                return true
+                expectedIndex = 0
+                val newValue = currentLevel.value!!.plus(1)
+                _currentLevel.value = newValue
+                generateSequence(currentLevel.value!!)
+            } else {
+                expectedIndex++
             }
+        }  else {
+            _gameState.value = GameState.GAME_OVER
         }
-        return false
     }
 
-    fun nextStep(input: Int) {
-        if(checkUserInput(input)) {
-            val newValue = currentLevel.value!!.plus(1)
-            _currentLevel.value = newValue
-            _gameState.value = GameState.IN_PROGRESS
-            generateSequence(currentLevel.value!!)
-        } else {
-            _gameState.value = GameState.GAME_OVER
-            // send score here
-        }
+    fun resetGame() {
+        _sequence.value = listOf<Int>()
+        _currentLevel.value = 1
+        currentLevel.value?.let { generateSequence(it) }
     }
 }
