dc023625b263a52babfd12c482534d80976b68dd
Jackson Lawrence <jlawrence1787@gmail.com>
2024-04-16 19:47:13 -0400

    added logout btn

--- mobile/app/src/main/java/com/cs4520/brainflex/view/Information.kt
+++ mobile/app/src/main/java/com/cs4520/brainflex/view/Information.kt
@@ -66,7 +66,7 @@ fun Title(){
             )
             .padding(10.dp)
             .fillMaxWidth(),
-        style = MaterialTheme.typography.subtitle1)
+        style = MaterialTheme.typography.subtitle1, color = MaterialTheme.colors.primary)
 }
 
 @Composable
@@ -137,7 +137,7 @@ fun AdditionalInfo(){
                 )
                 .padding(10.dp)
                 .fillMaxWidth(),
-            style = MaterialTheme.typography.caption)
+            style = MaterialTheme.typography.caption, color = MaterialTheme.colors.primary)
     }
 }
 
